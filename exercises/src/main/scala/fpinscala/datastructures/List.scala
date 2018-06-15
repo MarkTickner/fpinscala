77261e4f7594f253b61d0b491a8026b7db76354c
Tickner, Mark <Mark.Tickner@sky.uk>
2018-06-15 15:06:54 +0100

    Solution for exercise 3.20

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -155,6 +155,8 @@ object List { // `List` companion object. Contains functions for creating and wo
 
   // Exercise 3.20: Write a function `flatMap` that works like `map` except that the function given will return a list
   // instead of a single result
+  def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] =
+    concat(map(as)(f))
 
   // Exercise 3.21: Use `flatMap` to implement `filter`
 
