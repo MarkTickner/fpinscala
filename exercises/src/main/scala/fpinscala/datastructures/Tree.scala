12d5780c76a222469fffd7cfcfc6f90f28485003
Tickner, Mark <Mark.Tickner@sky.uk>
2018-07-13 16:05:23 +0100

    Solution for exercise 3.28

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -23,6 +23,10 @@ object Tree {
 
   // Exercise 3.28: Write a function `map`, analogous to the method of the same name on `List`, that modifies each element
   // in a tree with a given function
+  def map[A, B](tree: Tree[A])(f: A => B): Tree[B] = tree match {
+    case Leaf(v) => Leaf(f(v))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
 
   // Exercise 3.29: Generalize `size`, `maximum`, `depth`, and `map`, writing a new function `fold` that abstracts over
   // their similarities. Reimplement them in terms of this more general function
