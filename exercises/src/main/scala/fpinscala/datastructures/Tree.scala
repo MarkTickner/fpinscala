bc131e9ba52e16a33384a2229d7c1756c91b2ce2
Tickner, Mark <Mark.Tickner@sky.uk>
2018-09-28 14:35:12 +0100

    Solution for exercise 3.25

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -8,6 +8,12 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 object Tree {
 
   // Exercise 3.25: Write a function `size` that counts the number of nodes (leaves and branches) in a tree
+  def size[A](tree: Tree[A]): Int = {
+    tree match {
+      case Leaf(_) => 1
+      case Branch(l, r) => 1 + size(l) + size(r)
+    }
+  }
 
   // Exercise 3.26: Write a function `maximum` that returns the maximum element in a `Tree[Int]`
   def maximum(tree: Tree[Int]): Int = tree match {
