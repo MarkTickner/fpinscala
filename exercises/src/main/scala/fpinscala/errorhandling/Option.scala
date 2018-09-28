af3f517eedf1583e78a05efb0fe27610b0c90452
Tickner, Mark <Mark.Tickner@sky.uk>
2018-09-28 14:38:54 +0100

    Solution for exercise 4.4

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -60,7 +60,13 @@ object Option {
   // Exercise 4.4: Write a function `sequence` that combines a list of `Option`s into one `Option` containing a list of
   // all the `Some` values in the original list. If the original list contains `None` even once, the result of the
   // function should be `None`; otherwise the result should be `Some` with a list of all the values
-  def sequence[A](a: List[Option[A]]): Option[List[A]] = ???
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+    case Nil => Some(Nil)
+    case h :: t => h flatMap (hh => sequence(t) map (hh :: _))
+  }
+
+  def parseInts(a: List[String]): Option[List[Int]] =
+    sequence(a map (i => Try(i.toInt)))
 
   // Exercise 4.5: Implement this function
   def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = ???
